039593e513f984b849f608d98364ae3b1ed770c7
Eddington Rowellis <eddingtonibi@gmail.com>
2023-04-01 22:50:05 +0300

    commit tree f9c22bf7519c307db94dbfdf8794a4bbdcaccd4c

---
+++ tempCodeRunnerFile.rb
@@ -0,0 +1,32 @@
+class Person
+  attr_accessor :partner, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def get_married(person)
+    if person.is_a?(person)
+      self.partner = person
+      person.partner= person
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
+  end
+
+  class PartnerError < StandardError
+    def message
+      "fojo error"
+    end
+  end
+end
+
+
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
+
